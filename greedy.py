dbfdbb4afbc66e5c271709a5e0b849d52bb797d0
Tine Jozelj <tine@tjo.space>
2018-12-16 14:53:33 +0100

    fix sorting

--- greedy.py
+++ greedy.py
@@ -1,6 +1,6 @@
 def greedy_multiple_knapsacks(capacities, weights, benefits):
     numOfItems = len(weights) #number of passed items
-    relativeBenefits = []  #list with 
+    relativeBenefits = []  #list with items
     knapsacks = []  # list of m knapsacks 
     numOfKnap = len(capacities) # m
     mkp = 0 #final value in the knapsack(s)
@@ -11,13 +11,15 @@ def greedy_multiple_knapsacks(capacities, weights, benefits):
     #relative benefits are calculated
     for i in range(numOfItems):
         relativeBenefits.append((round(benefits[i]/weights[i],2), weights[i], benefits[i]))
-        
-    #list is sorted from high up to low values    
-    relativeBenefits.sort(reverse= True)  
+    
+    # Sorting list
+    # First value in tuple is relativeBenefit whitch should be sorted highest
+    # Second value is weight which should be sorted by lowest first (that's why we do -value)  
+    relativeBenefits.sort(reverse=True, key=lambda tup:(tup[0], -tup[1]))  
    
     #each item is tried to fit in one knapsack
     #when the capacity of one knapsack is big enough - the item is put inside and is removed from the itemlist
-    for i in range(numOfItems):
+    for _ in range(numOfItems):
         for j in range(len(knapsacks)):
             if capacities[j] > 0:
                 if capacities[j] >= relativeBenefits[0][1]:
--- tests/test_greedy.py
+++ tests/test_greedy.py
@@ -4,7 +4,7 @@ import greedy
 class TestGreedy(unittest.TestCase):
 
     def test_greedy_single(self):
-        self.assertEqual(greedy.greedy_multiple_knapsacks([15],[2,3,5,7,1,4,1], [10,5,15,7,6,18,3]), 57)
+        self.assertEqual(greedy.greedy_multiple_knapsacks([15],[2,3,5,7,1,4,1], [10,5,15,7,6,18,3]), 52)
 
     def test_greedy_mutiple(self):
         self.assertEqual(greedy.greedy_multiple_knapsacks([10, 15],[5,2,24,4], [13,10,20,8]), 31)
