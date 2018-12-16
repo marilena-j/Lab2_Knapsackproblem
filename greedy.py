c3c9d4ddc85b2d0fa6348f9674676b29268c8f81
Tine Jozelj <tine@tjo.space>
2018-12-16 16:47:14 +0100

    nice thing that doese not work

--- greedy.py
+++ greedy.py
@@ -1,6 +1,22 @@
-def greedy_multiple_knapsacks(capacities, weights, benefits):
-    numOfItems = len(weights) #number of passed items
-    relativeBenefits = []  #list with items
+class Item:
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+        self.relative_benefit = round(value/weight, 2)
+
+    def __gt__(self, item):
+        return self.relative_benefit > item.relative_benefit and self.weight < item.weight
+
+    def __eq__(self, item):
+        return self.relative_benefit == item.relative_benefit and self.weight == item.weight
+
+    def __repr__(self):
+        return '<Item v:%i w:%i rb:%i>' % (self.value, self.weight, self.relative_benefit)
+
+"""
+Basic greedy multiple knapsacks algorithem
+"""
+def greedy_multiple_knapsacks(capacities, items):
     knapsacks = []  # list of m knapsacks 
     numOfKnap = len(capacities) # m
     mkp = 0 #final value in the knapsack(s)
@@ -8,27 +24,32 @@ def greedy_multiple_knapsacks(capacities, weights, benefits):
     for i in range(numOfKnap):
         knapsacks.append([]) #each knapsack gets a list for the items
     
-    #relative benefits are calculated
-    for i in range(numOfItems):
-        relativeBenefits.append((round(benefits[i]/weights[i],2), weights[i], benefits[i]))
-    
     # Sorting list
     # First value in tuple is relativeBenefit whitch should be sorted highest
     # Second value is weight which should be sorted by lowest first (that's why we do -value)  
-    relativeBenefits.sort(reverse=True, key=lambda tup:(tup[0], -tup[1]))  
+    items.sort(reverse=True)
    
     #each item is tried to fit in one knapsack
     #when the capacity of one knapsack is big enough - the item is put inside and is removed from the itemlist
-    for _ in range(numOfItems):
+    for _ in range(len(items)):
+        current_item = items[0]
         for j in range(len(knapsacks)):
-            if capacities[j] > 0:
-                if capacities[j] >= relativeBenefits[0][1]:
-                    knapsacks[j].append(relativeBenefits[0])
-                    capacities[j] -= relativeBenefits[0][1]
-                    mkp +=  relativeBenefits[0][2]#total value in the bag(s)
-                    break                
-        del relativeBenefits[0]
+            if capacities[j] >= current_item.weight:
+                knapsacks[j].append(current_item)
+                capacities[j] -= current_item.weight
+                mkp += items[0].value # total value in the bag(s)
+                break                
+        del items[0]
             
     #complete list of knapsacks content and final value
-    print (knapsacks)
-    return mkp
+    return mkp, knapsacks
+
+"""
+Neighbouring algorithem
+"""
+def neighbouring_search(capacities, weights, benefits):
+    resoult_value, result_knapsacks = greedy_multiple_knapsacks(capacities, weights, benefits)
+
+#    while True:
+
+    return resoult_value, result_knapsacks
--- tests/test_greedy.py
+++ tests/test_greedy.py
@@ -1,10 +1,23 @@
 import unittest
 import greedy
 
+def create_items(weights, values):
+    return [ greedy.Item(values[i], weights[i]) for i in range(len(weights))]
+
 class TestGreedy(unittest.TestCase):
 
     def test_greedy_single(self):
-        self.assertEqual(greedy.greedy_multiple_knapsacks([15],[2,3,5,7,1,4,1], [10,5,15,7,6,18,3]), 52)
+        items = create_items([2,3,5,7,1,4,1], [10,5,15,7,6,18,3])
+
+        print()
+        print(items)
+        items.sort(reverse=True)
+        print(items)
+
+        result = greedy.greedy_multiple_knapsacks([15], items)
+        self.assertEqual(result[0], 52)
 
     def test_greedy_mutiple(self):
-        self.assertEqual(greedy.greedy_multiple_knapsacks([10, 15],[5,2,24,4], [13,10,20,8]), 31)
+        items = create_items([5,2,24,4], [13,10,20,8])
+        result = greedy.greedy_multiple_knapsacks([10, 15], items)
+        self.assertEqual(result[0], 31)
